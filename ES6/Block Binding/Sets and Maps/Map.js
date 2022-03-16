544f72b65e723d68681d79260782df90647d1f37
Karthik R <iamjustkarthik@gmail.com>
2022-03-16 20:43:13 +0530

    commit

---
+++ ES6/Block Binding/Sets and Maps/Map.js
@@ -0,0 +1,11 @@
+function map(){
+    var map=new Map();
+    console.log(map.size);
+    map.set("age",35);
+    console.log(map.size);
+    console.log(map);
+    //var ralph={'name':'Ralph'};
+    //map.set(ralph,29);
+   // console.log(map);
+}
+map()
--- ES6/Block Binding/Sets and Maps/sets.js
+++ ES6/Block Binding/Sets and Maps/sets.js
@@ -4,3 +4,6 @@ set.add("5");
 set.add("8");
 set.add(5);//ignored
 console.log(set.size);
+let key={};
+set.add(key);
+console.log(set)
