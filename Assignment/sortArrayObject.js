66fcc39427fd2545675070e7a1618f24dad2ce9f
Karthik R <iamjustkarthik@gmail.com>
2022-04-13 17:03:46 +0530

    Sort Array Objects

--- Assignment/sortArrayObject.js
+++ Assignment/sortArrayObject.js
@@ -15,42 +15,16 @@ let array=[
         firstName:"Gina",
         lastName:"Linetti"
     }
-]
-// let array1=array.map((object,index)=>{
-//     let array2=array.map((obj,ind)=>{
-
-//     })
-// })
-
-array.sort((a,b)=>{
-    console.log(a.firstName,b.firstName)
-    return a.firstName - b.firstName;
-})
-console.log(array)
-
-let employees = [
-    {
-        firstName: 'John',
-        lastName: 'Doe',
-        age: 27,
-        joinedDate: 'December 15, 2017'
-    },
-
-    {
-        firstName: 'Ana',
-        lastName: 'Rosy',
-        age: 25,
-        joinedDate: 'January 15, 2019'
-    },
-
-    {
-        firstName: 'Zion',
-        lastName: 'Albert',
-        age: 30,
-        joinedDate: 'February 15, 2011'
-    }
 ];
-employees.sort((a, b) => {
-    return a.age - b.age;
+array.sort((second,first) => {
+    let fName=first.firstName;
+    let sName=second.firstName;
+    if (fName < sName) {
+        return 1;
+    }
+    if (fName > sName) {
+        return -1;
+    }
+    return 0;
 });
-console.log(employees)
+console.log(array)
