b6c28a76540925d64123f7791125c2f82b6aadb2
Karthik R <iamjustkarthik@gmail.com>
2022-04-13 10:10:12 +0530

    changes

--- Assignment/sortArrayObject.js
+++ Assignment/sortArrayObject.js
@@ -27,3 +27,30 @@ array.sort((a,b)=>{
     return a.firstName - b.firstName;
 })
 console.log(array)
+
+let employees = [
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 27,
+        joinedDate: 'December 15, 2017'
+    },
+
+    {
+        firstName: 'Ana',
+        lastName: 'Rosy',
+        age: 25,
+        joinedDate: 'January 15, 2019'
+    },
+
+    {
+        firstName: 'Zion',
+        lastName: 'Albert',
+        age: 30,
+        joinedDate: 'February 15, 2011'
+    }
+];
+employees.sort((a, b) => {
+    return a.age - b.age;
+});
+console.log(employees)
